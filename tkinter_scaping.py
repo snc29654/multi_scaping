e553e2fbc0ad1e21c95c29030f61b3754b825bb7
snc29654 <snc29654l@nifty.com>
2022-10-14 21:56:08 +0900

    url追加

--- tkinter_scaping.py
+++ tkinter_scaping.py
@@ -95,6 +95,9 @@ urls = [
     "https://news.yahoo.co.jp/categories/sports",
     "https://news.yahoo.co.jp/categories/entertainment", 
     "https://news.yahoo.co.jp/categories/business", 
+    "https://news.yahoo.co.jp/categories/it", 
+    "https://news.yahoo.co.jp/categories/world", 
+    "https://news.yahoo.co.jp/categories/science", 
     "https://news.yahoo.co.jp/topics/top-picks"
 ]
 
